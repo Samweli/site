6b4aaf0cfb1d3662527b09157c3cd76d904122e9
samweli <smwltwesa6@gmail.com>
2024-05-07 10:51:23 +0300

    new feature row

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -1,60 +1,60 @@
 var store = [{
         "title": "Climate Action Hackathon",
-        "excerpt":"The Last Mile That was the buzzword for the hackathon carrying a meaning that after all the years of collecting, organizing and storing climate data, it is now time for technology to make use of them and provide cutting edge applications. The United Nations Development Programme (UNDP) launched this for...","categories": [],
+        "excerpt":"![](https://raw.githubusercontent.com/samweli/jekyll-now/master/images/undp-hackathon.jpeg) ## The Last Mile That was the buzzword for the hackathon carrying a meaning that after all the years of collecting, organizing and storing climate data, it is now time for technology to make use of them and provide cutting edge applications. The United Nations Development Programme (UNDP) launched...","categories": [],
         "tags": [],
         "url": "/Climate-Action-Hackathon/",
         "teaser": null
       },{
         "title": "Collection of Daladala Routes",
-        "excerpt":"Map of public transport in Dar es salaam link Dar es salaam Transport Dar es Salaam’s public transport systems is mainly centred around Daladala buses. In addition there is a train, ferry, and BRT system. As part of Ramani Huria team, In 2015 We worked together with ally (mobility based...","categories": [],
+        "excerpt":"![Dar es salaam transport routes (Bus routes in red)](https://raw.githubusercontent.com/samweli/jekyll-now/master/images/bus_routes.png) _Map of public transport in Dar es salaam [link](https://api.mapbox.com/styles/v1/samtwesa/cijptupyz002d94kq81o26eww.html?fresh=true&title=true&access_token=pk.eyJ1Ijoic2FtdHdlc2EiLCJhIjoiZTc1OTQ4ODE0ZmY2MzY0MGYwMDNjOWNlYTYxMjU4NDYifQ.F1zCcOYqpXWd4C9l9xqvEQ#19.3/-6.816097/39.280324/0)_ ## Dar es salaam Transport Dar es Salaam's public transport systems is mainly centred around Daladala buses. In addition there is a train, ferry, and BRT system. As part of Ramani Huria...","categories": [],
         "tags": [],
         "url": "/Daladala-Routes/",
         "teaser": null
       },{
         "title": "Code for Resilience",
-        "excerpt":"Code for Resilience Dar es salaam Dar es Salaam and Stone Town are the two primary cities of Mainland Tanzania and Zanzibar, with populations of 5.5 million and 150,000 respectively. Each of these cities and their country have unique challenges and hazards. Dar es Salaam for instance is challenged by...","categories": [],
+        "excerpt":"![Code for Resilience Dar es salaam kickoff](https://raw.githubusercontent.com/samweli/jekyll-now/master/images/code-for-resilience.png) ## Code for Resilience Dar es salaam Dar es Salaam and Stone Town are the two primary cities of Mainland Tanzania and Zanzibar, with populations of 5.5 million and 150,000 respectively. Each of these cities and their country have unique challenges and hazards....","categories": [],
         "tags": [],
         "url": "/Code-for-Resilience/",
         "teaser": null
       },{
         "title": "Developing Flood Maps",
-        "excerpt":"Floods in Dar es salaam Dar es salaam is one of the fastest growing cities in Africa, it has a 5 million population and 5.6% percent growth rate per year, it is also one of the most important economic centers in Tanzania regions. This indicates that the city should get...","categories": [],
+        "excerpt":"![](https://raw.githubusercontent.com/samweli/jekyll-now/master/images/flood_map.png) ## Floods in Dar es salaam Dar es salaam is one of the fastest growing cities in Africa, it has a 5 million population and 5.6% percent growth rate per year, it is also one of the most important economic centers in Tanzania regions. This indicates that the city...","categories": [],
         "tags": [],
         "url": "/Flood-Maps/",
         "teaser": null
       },{
         "title": "Isochrone Maps Creation in QGIS",
-        "excerpt":"example isochrone map showing hospitals accessibility times in minutes by car Isochrone Map I created a plugin to help map creators to use QGIS in producing isochrone maps, which are used to show accessibility of different facilities in service areas. This tool extends QGIS functionality in isochrone map creation. The...","categories": [],
+        "excerpt":"![](https://raw.githubusercontent.com/samweli/jekyll-now/master/images/isochrone_map.png) _example isochrone map showing hospitals accessibility times in minutes by car_ ## Isochrone Map I created a plugin to help map creators to use QGIS in producing isochrone maps, which are used to show accessibility of different facilities in service areas. This tool extends QGIS functionality in isochrone map...","categories": [],
         "tags": [],
         "url": "/Isochrone-Maps/",
         "teaser": null
       },{
         "title": "Developing Twaamtaro (Adopt a Drain)",
-        "excerpt":"Twaamtaro Tanzania Online platform that tracks local drain cleanliness &amp; information on solid waste deposits. Inspired by Adopt a Drain from Code for America!. Background Waste cleanness in Dar es salaam drains is rarely done, and when it is done, there is lack of dissemination of the work and drains...","categories": [],
+        "excerpt":"![](https://raw.githubusercontent.com/samweli/jekyll-now/master/images/twaamtaro_logo.png) ## Twaamtaro Tanzania Online platform that tracks local drain cleanliness & information on solid waste deposits. Inspired by Adopt a Drain from Code for America!. ### Background Waste cleanness in Dar es salaam drains is rarely done, and when it is done, there is lack of dissemination of the...","categories": [],
         "tags": [],
         "url": "/Twaamtaro/",
         "teaser": null
       },{
         "title": "The Making of Trash Data Visualization",
-        "excerpt":"Online Trash Map Introduction In August 2018 HOT together with Nipe Fagio organized waste data collection activity, using more than 100 students/mappers to locate and collect information about the types of piles in the Dar es salaam City. The aim of this activity was to raise awareness to the responsible...","categories": [],
+        "excerpt":"![](https://raw.githubusercontent.com/samweli/jekyll-now/master/images/trash_map.png) _Online Trash Map_ ## Introduction In August 2018 [HOT](https://www.hotosm.org) together with [Nipe Fagio](http://nipefagio.co.tz) organized waste data collection activity, using more than 100 students/mappers to locate and collect information about the types of piles in the Dar es salaam City. The aim of this activity was to raise awareness to...","categories": [],
         "tags": [],
         "url": "/Trash-Map/",
         "teaser": null
       },{
         "title": "Using AI in Trash Detection",
-        "excerpt":"Trash detection web application interface Building web application to detect trash in images After trash mapping activity here, some of the collected trash images didn’t contain waste, this occured because some of the images were for legal dump sites. During the collection activity the data wasn’t tagged whether it contained...","categories": [],
+        "excerpt":"![](https://raw.githubusercontent.com/samweli/jekyll-now/master/images/trash-detector.png) _Trash detection web application interface_ ## Building web application to detect trash in images After trash mapping activity [here](http://samweli.github.io/Trash-Map/), some of the collected trash images didn't contain waste, this occured because some of the images were for legal dump sites. During the collection activity the data wasn't tagged whether...","categories": [],
         "tags": [],
         "url": "/AI-Trash-Detection/",
         "teaser": null
       },{
         "title": "Converting ODK location data to WKT in QGIS",
-        "excerpt":"Plugin interface Loading ODK location data in QGIS ODK is now widely used in collecting location data, one of its use case is the project called Ramani Huria (Open Map) which use ODK to map Dar es salaam city drains. One of the challenges in using ODK to collect linestring...","categories": [],
+        "excerpt":"![](https://raw.githubusercontent.com/samweli/jekyll-now/master/images/odktrace2wkt.png) _Plugin interface_ ## Loading ODK location data in QGIS ODK is now widely used in collecting location data, one of its use case is the project called Ramani Huria (Open Map) which use ODK to map Dar es salaam city drains. One of the challenges in using ODK to...","categories": [],
         "tags": [],
         "url": "/ODKTrace2WKT/",
         "teaser": null
       },{
         "title": "Visualizing Dar es salaam City Drone Imagery",
-        "excerpt":"Drone Imagery Map Visualizing Dar es salaam drone imagery 2 At the end of 2017 The World Bank Tanzania helped in collecting drone imagery in Dar es salaam as per request of DART (Dar Rapid Transit). The activity covered all areas that are nearby all the planned BRT (Bus Rapid...","categories": [],
+        "excerpt":"![](https://raw.githubusercontent.com/samweli/jekyll-now/master/images/dar-drone-imagery-viz.png) _Drone Imagery Map_ ## Visualizing Dar es salaam drone imagery 2 At the end of 2017 The World Bank Tanzania helped in collecting drone imagery in Dar es salaam as per request of [DART](https://www.dart.go.tz/) (Dar Rapid Transit). The activity covered all areas that are nearby all the planned BRT...","categories": [],
         "tags": [],
         "url": "/Drone-Imagery-Viz/",
         "teaser": null
